726b831d8cfeb3f3a9f09505e1c2c8d0eecbcfa1
Esduardo Del Aguila <daguilae@yahoo.com>
2023-05-17 10:01:02 -0600

    Agregando funcionalidad de la bitacora CRUD secuencial

---
+++ Juegos/Robot/Juego_Robot/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+//#include "jugador.h"
+//#include "celdas.h"
+//#include "mapajuego.h"
+
+using namespace std;
+
+int main()
+{
+    //aplicativo elaborado en el curso de Programaciòn I Universidad Mariano Galvez de Guatemala"
+    cout << "Hola Mundo";
+    return 0;
+}
+
